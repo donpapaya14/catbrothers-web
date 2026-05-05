bf8a26c981e1d10ec9d14bc936e6254fae20e32b
Vladys <vladys.z96@gmail.com>
2026-05-05 15:44:03 +0200

    feat: update Amazon tag to vds96-20 (amazon.com)

--- scripts/generate_article.py
+++ scripts/generate_article.py
@@ -34,7 +34,7 @@ CATEGORY_NAMES = {
     "entertainment": "Cat Fun Facts",
 }
 
-AMAZON_TAG = os.getenv("AMAZON_TAG", "vladys-21")
+AMAZON_TAG = os.getenv("AMAZON_TAG", "vds96-20")
 SITE_URL = os.getenv("SITE_URL", "https://catbrothers-web.vercel.app")
 BRAND = os.getenv("BRAND", "CatBrothers")
 
